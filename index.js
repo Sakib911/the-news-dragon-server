87bd223d43e49ccaf8f9cbfb971b718d27a02361
Sakib911 <sakibhasan911@gmail.com>
2024-01-08 01:56:05 +0600

    vercel added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
-
+const news = require('./data/news.json');
 app.use(cors())
 app.get('/', (req, res) =>
     res.send('Dragon Is Running')
@@ -12,6 +12,25 @@ app.get('/', (req, res) =>
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(newses => newses._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == 0) {
+        res.send(news)
+    } else {
+        const selectedCategory = news.filter(categories => categories.category_id === id);
+        res.send(selectedCategory);
+    }
+
 })
 
 app.listen(port, () =>
