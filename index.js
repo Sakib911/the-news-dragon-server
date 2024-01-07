c22d0b01729f11ee3205de6cee2ff2f19eadebac
Sakib911 <sakibhasan911@gmail.com>
2024-01-08 02:21:04 +0600

    port updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
 const news = require('./data/news.json');
